71cb7cbef1f978151ffa2b343fc3fa4f9306562f
theslof <theslof@gmail.com>
2018-05-25 11:24:34 +0200

    1.2 - Now pulls user data from Firebase and passes a Game object to Playfield

--- src/pages/setup-local-game/setup-local-game.ts
+++ src/pages/setup-local-game/setup-local-game.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {PlayfieldPage} from "../playfield/playfield";
+import {FirebaseProvider} from "../../providers/firebase/firebase";
+import {Game, User} from "../../lib/interfaces";
 
 @IonicPage()
 @Component({
@@ -8,17 +10,26 @@ import {PlayfieldPage} from "../playfield/playfield";
   templateUrl: 'setup-local-game.html',
 })
 export class SetupLocalGamePage {
-  playerName: string = "Example User";
+  user: User;
+  playerName: string = "Loading User...";
   opponentName: string = "Player 2";
+  loading: boolean = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private alertCtrl: AlertController, private db: FirebaseProvider) {
+    db.getCurrentUser().subscribe((user: User) => {
+      console.log("Got user");
+      if (user) {
+        this.playerName = user.displayName;
+        this.user = user;
+        this.loading = false;
+      }
+    },err => {
+      console.log(err.toString());
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetupLocalGamePage');
-  }
-
-  public changeOpponentName(){
+  public changeOpponentName() {
     let prompt = this.alertCtrl.create({
       title: 'Change name',
       message: "Enter a name for Player 2:",
@@ -50,8 +61,18 @@ export class SetupLocalGamePage {
     prompt.present();
   }
 
-  public startGame(){
-    this.navCtrl.push(PlayfieldPage, {resume: false});
+  public startGame() {
+    this.loading = true;
+    let game: Game = {} as Game;
+    game.player1 = this.user.uid;
+    game.player2 = this.opponentName;
+    game.activePlayer = 0;
+    game.state = 'init';
+    game.type = 'local';
+    this.db.createGame(game).then(value => {
+      this.loading = false;
+      this.navCtrl.push(PlayfieldPage, {game: game});
+    });
   }
 
 }
