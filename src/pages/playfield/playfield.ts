b22063c0d9ced6e44aa7d4b482bb2d129a6ed460
fr9 <eliasf@me.com>
2018-05-30 10:09:37 +0200

    15.1 Spellogik - New bot player and fixed UI

--- src/pages/playfield/playfield.ts
+++ src/pages/playfield/playfield.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Popover, PopoverController} from 'ionic-angular';
+import {PopoverMenuData, PopoverPage} from "../popover/popover";
+import {fakeAsync} from "@angular/core/testing";
+import {Game} from "../../lib/interfaces";
+
 
 @IonicPage()
 @Component({
@@ -7,6 +11,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'playfield.html',
 })
 export class PlayfieldPage {
+  menu: Popover;
+  menuData: PopoverMenuData = {
+    choices: [
+      {icon: "hammer", text: "Clear"},
+      {icon: "hammer", text: "Save grid"},
+      {icon: "hammer", text: "Load grid"},
+      {icon: "hammer", text: "Step move"},
+      {icon: "hammer", text: "Switch player"},
+      {icon: "hammer", text: "Palay AI"},
+      {icon: "close", text: "Surrender"}
+    ],
+    callback: (index: number) => {this.onOptionsItemSelected(index);}
+  };
 
   gameGrid: string[][];
   width=7;
@@ -17,7 +34,7 @@ export class PlayfieldPage {
     yellow: "assets/imgs/coin-yellow.svg"
   };
 
-  placeCoinPosition: number = 0;
+  placeCoinPosition: number = 3;
   dropping: boolean = false;
   playerOneTurn: boolean = true;
   placedY: number;
@@ -28,8 +45,57 @@ export class PlayfieldPage {
   stepX: number;
   stepY: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  win: boolean = false;
+  gridCopy: string[][];
+  aiBot: boolean = false;
+
+  game: any;
+  player1Name: string;
+  player2Name: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private popCtrl: PopoverController) {
+    this.game = this.navParams.get("game") as Game;
+
+    this.menu = this.popCtrl.create(PopoverPage, this.menuData);
     this.drawPlayfield();
+  }
+
+  //----------------------------------------------
+  // Popover
+  //
+
+  /*
+  <button item-end ion-button small round (click)="nextPlayerTurn()">Switch</button>
+    <button item-end ion-button small round (click)="clearGrid()">Clear</button>
+    <button item-end ion-button small round (click)="JSONstringifyGrid()">Save</button>
+    <button item-end ion-button small round (click)="loadGrid()">Load</button>
+    <button item-end ion-button small round (click)="stepFromLoadedGrid()">Step</button>
+   */
+
+  public onOptionsItemSelected(id: number){
+    console.log("Callback: " + id);
+    switch (id) {
+      case 0:
+        this.clearGrid();
+        break;
+      case 1:
+        this.JSONstringifyGrid();
+        break;
+      case 2:
+        this.loadGrid();
+        break;
+      case 3:
+        this.stepFromLoadedGrid();
+        break;
+      case 4:
+        this.nextPlayerTurn();
+        break;
+      case 5:
+        this.aiBot = !this.aiBot;
+        break;
+      default:
+        console.log("Unknown choice");
+    }
   }
 
   //----------------------------------------------
@@ -38,7 +104,7 @@ export class PlayfieldPage {
 
   // Get display names from db.
   private getPlayers() {
-    return "Player 1 - Player 2"
+    return "Player 1 - Player 2";
   }
 
   //----------------------------------------------
@@ -57,18 +123,17 @@ export class PlayfieldPage {
 
   // Returns the hidden button size. Based on placment coin.
   private getHiddenMoveButtonsPosition() {
-    console.log((100/this.width)*this.placeCoinPosition + "%");
-    return ((100/this.width)*this.placeCoinPosition)-10 + "%";
+    return ((100/this.width)*this.placeCoinPosition)-45 + "%";
   }
 
   //
   private getMoveRightWidth() {
-    return ((100/this.width)*this.placeCoinPosition)-10 + "%";
+    return (100/this.width)*(this.placeCoinPosition+1) + "%";
   }
 
   //
   private getMoveLeftWidth() {
-    return 90-(((100/this.width)*this.placeCoinPosition)) + "%";
+    return 80-(100/this.width)*(this.placeCoinPosition+1) + "%";
   }
 
 
@@ -128,6 +193,7 @@ export class PlayfieldPage {
     if (this.getPlacedY() <= 5) {
       this.dropping = true;
       this.dropCoin(this.placeCoinPosition, this.getPlacedY(), this.getNextCoin());
+      if(this.winCheck(this.getNextCoin())) this.win = true;
     }
   }
 
@@ -135,6 +201,7 @@ export class PlayfieldPage {
   private nextPlayerTurn() {
     this.switchTurn();
     this.dropping = false;
+    if (this.getNextCoin() === this.coins.red && this.aiBot) this.aiMove();
   }
 
   // Switch player
@@ -148,6 +215,231 @@ export class PlayfieldPage {
     return this.coins.red;
   }
 
+  private newGame() {
+    this.clearGrid();
+    this.dropping = false;
+    this.playerOneTurn = true;
+    this.placeCoinPosition = 3;
+  }
+
+  //----------------------------------------------
+  // Win check
+  //
+
+  private winCheck(player :string) {
+
+    // verticalCheck
+    for (let y=0; y<this.height-3; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (
+          this.gameGrid[y][x] === player &&
+          this.gameGrid[y + 1][x] === player &&
+          this.gameGrid[y + 2][x] === player &&
+          this.gameGrid[y + 3][x] === player
+        ) return true;
+      }
+    }
+
+    // horizontalCheck
+    for (let x=0; x<this.width-3; x++) {
+      for (let y = 0; y < this.height; y++) {
+        if (
+          this.gameGrid[y][x] === player &&
+          this.gameGrid[y][x + 1] === player &&
+          this.gameGrid[y][x + 2] === player &&
+          this.gameGrid[y][x + 3] === player
+        ) return true;
+      }
+    }
+
+    // ascendingDiagonalCheck
+    for (let x=3; x<this.width; x++) {
+      for (let y = 0; y < this.height-3; y++) {
+        if (
+          this.gameGrid[y][x] === player &&
+          this.gameGrid[y + 1][x - 1] === player &&
+          this.gameGrid[y + 2][x - 2] === player &&
+          this.gameGrid[y + 3][x - 3] === player
+        ) return true;
+      }
+    }
+
+    // descendingDiagonalCheck
+    for (let x=3; x<this.width; x++) {
+      for (let y = 3; y < this.height; y++) {
+        if (
+          this.gameGrid[y][x] === player &&
+          this.gameGrid[y - 1][x - 1] === player &&
+          this.gameGrid[y - 2][x - 2] === player &&
+          this.gameGrid[y - 3][x - 3] === player
+        ) return true;
+      }
+    }
+
+    // No win
+    return false;
+
+  }
+
+  //----------------------------------------------
+  // AI player
+  //
+
+  private aiMove() {
+
+    let currentPlayer = this.getNextCoin();
+    let otherPlayer = this.coins.yellow;
+    let result: boolean;
+    let deepGridCopy = JSON.parse(JSON.stringify(this.gameGrid));
+
+    if (currentPlayer === this.coins.yellow) otherPlayer = this.coins.red;
+
+    for (let x = 0; x < this.width; x++) {
+      this.gridCopy = JSON.parse(JSON.stringify(this.gameGrid));
+      for (let y = this.height - 1; y >= 0; y--) {
+        if (this.gridCopy[y][x] === this.coins.blank) {
+          this.gridCopy[y][x] = currentPlayer;
+          result = this.aiTestOutcome(currentPlayer);
+          console.log(x + ", " + y + " - " + result);
+          if (result) {
+            console.log("100% @ " + x + ", " + y);
+            this.aiPlace(x);
+            return null;
+          }
+          break;
+        }
+      }
+    }
+
+    for (let i=0; i<1; i++) {
+
+      for (let x = 0; x < this.width; x++) {
+        this.gridCopy = JSON.parse(JSON.stringify(this.gameGrid));
+        for (let y = this.height - 1; y >= 0; y--) {
+          if (this.gridCopy[y][x] === this.coins.blank) {
+            this.gridCopy[y][x] = otherPlayer;
+            result = this.aiTestOutcome(otherPlayer);
+            console.log(x + ", " + y + " - " + result);
+            if (result) {
+              console.log("99% @ " + x + ", " + y);
+              //deepGridCopy = JSON.parse(JSON.stringify(this.gridCopy));
+              this.aiPlace(x);
+              return null;
+            }
+            break;
+          }
+        }
+      }
+
+    }
+
+    let randomX;
+    let loopVar =  true;
+    let randomY;
+    while(loopVar) {
+
+      randomX = Math.floor(Math.random() * this.width);
+      for (randomY = this.height - 1; randomY >= 0; randomY--) {
+
+        if (this.gridCopy[randomY][randomX] === this.coins.blank) {
+          this.gridCopy = JSON.parse(JSON.stringify(this.gameGrid));
+          this.gridCopy[randomY][randomX] = currentPlayer;
+          deepGridCopy = JSON.parse(JSON.stringify(this.gridCopy));
+          break;
+        }
+
+      }
+
+
+      for (let x = 0; x < this.width; x++) {
+        this.gridCopy = JSON.parse(JSON.stringify(deepGridCopy));
+        for (let y = this.height - 1; y >= 0; y--) {
+          if (this.gridCopy[y][x] === this.coins.blank) {
+            this.gridCopy[y][x] = otherPlayer;
+            result = this.aiTestOutcome(otherPlayer);
+            console.log(x + ", " + y + " - " + result);
+            if (result) {
+              loopVar = true;
+              console.log("Bad move predicted 99%  @ " + randomX + ", " + randomY);
+              y = 0;
+              x = this.width;
+              break;
+            } else {
+              loopVar = false;
+            }
+            break;
+          }
+        }
+      }
+
+    }
+
+    console.log("86% @ " + randomX + ", " + randomY);
+    this.aiPlace(randomX);
+
+  }
+
+  private aiPlace(x: number) {
+    this.placeCoinPosition = x;
+    this.moveDown();
+    //this.nextPlayerTurn();
+  }
+
+  private aiTestOutcome(player :string) {
+
+    // verticalCheck
+    for (let y = 0; y < this.height - 3; y++) {
+      for (let x = 0; x < this.width; x++) {
+        if (
+          this.gridCopy[y][x] === player &&
+          this.gridCopy[y + 1][x] === player &&
+          this.gridCopy[y + 2][x] === player &&
+          this.gridCopy[y + 3][x] === player
+        ) return true;
+      }
+    }
+
+    // horizontalCheck
+    for (let x = 0; x < this.width - 3; x++) {
+      for (let y = 0; y < this.height; y++) {
+        if (
+          this.gridCopy[y][x] === player &&
+          this.gridCopy[y][x + 1] === player &&
+          this.gridCopy[y][x + 2] === player &&
+          this.gridCopy[y][x + 3] === player
+        ) return true;
+      }
+    }
+
+    // ascendingDiagonalCheck
+    for (let x = 3; x < this.width; x++) {
+      for (let y = 0; y < this.height - 3; y++) {
+        if (
+          this.gridCopy[y][x] === player &&
+          this.gridCopy[y + 1][x - 1] === player &&
+          this.gridCopy[y + 2][x - 2] === player &&
+          this.gridCopy[y + 3][x - 3] === player
+        ) return true;
+      }
+    }
+
+    // descendingDiagonalCheck
+    for (let x = 3; x < this.width; x++) {
+      for (let y = 3; y < this.height; y++) {
+        if (
+          this.gridCopy[y][x] === player &&
+          this.gridCopy[y - 1][x - 1] === player &&
+          this.gridCopy[y - 2][x - 2] === player &&
+          this.gridCopy[y - 3][x - 3] === player
+        ) return true;
+      }
+    }
+
+    // No win
+    return false;
+  }
+
+
   //----------------------------------------------
   // Dev tools
   //
@@ -158,6 +450,7 @@ export class PlayfieldPage {
   }
 
   private clearGrid() {
+    this.win = false;
     this.drawPlayfield();
     this.stepX = this.width-1;
     this.stepY = this.height-1;
